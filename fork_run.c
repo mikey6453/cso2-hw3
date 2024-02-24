f9e3f971128e69fec805bc4e145dfbb4d705f162
Michael <tvq3bn@virginia.edu>
2024-02-23 22:47:37 -0500

    new changes

--- fork_run.c
+++ fork_run.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
 
-char *captureCommandOutput(const char *command) {
-    int pipefd[2];
-    if (pipe(pipefd) != 0) {
-        perror("pipe");
-        return NULL;
+static char *readOutputFromPipe(int fd) {
+    char *output = NULL;
+    size_t size = 0, capacity = 4096;
+    ssize_t bytes_read;
+    output = (char *)malloc(capacity);
+    if (!output) return NULL;
+
+    while ((bytes_read = read(fd, output + size, capacity - size - 1)) > 0) {
+        size += bytes_read;
+        if (size >= capacity - 1) {
+            capacity *= 2;
+            char *newOutput = realloc(output, capacity);
+            if (!newOutput) {
+                free(output);
+                return NULL;
+            }
+            output = newOutput;
+        }
     }
+    output[size] = '\0';
+    return output;
+}
+
+char *getoutput(const char *command) {
+    int pipefd[2];
+    if (pipe(pipefd) != 0) return NULL;
 
     pid_t pid = fork();
     if (pid == -1) {
-        perror("fork");
         close(pipefd[0]);
         close(pipefd[1]);
         return NULL;
@@ -21,55 +40,47 @@ char *captureCommandOutput(const char *command) {
         close(pipefd[0]);
         dup2(pipefd[1], STDOUT_FILENO);
         execl("/bin/sh", "sh", "-c", command, NULL);
-        perror("execl");
         _exit(EXIT_FAILURE);
     }
 
     close(pipefd[1]);
-    char *output = NULL;
-    size_t size = 0, capacity = 4096;
-    output = (char *)malloc(capacity);
-    if (!output) {
-        perror("malloc");
-        close(pipefd[0]);
-        waitpid(pid, NULL, 0);
-        return NULL;
-    }
-
-    ssize_t bytes_read;
-    while ((bytes_read = read(pipefd[0], output + size, capacity - size - 1)) > 0) {
-        size += bytes_read;
-        if (size >= capacity - 1) {
-            capacity *= 2;
-            output = (char *)realloc(output, capacity);
-            if (!output) {
-                perror("realloc");
-                close(pipefd[0]);
-                waitpid(pid, NULL, 0);
-                return NULL;
-            }
-        }
-    }
-    output[size] = '\0';
+    char *output = readOutputFromPipe(pipefd[0]);
     close(pipefd[0]);
     waitpid(pid, NULL, 0);
     return output;
 }
 
-char *captureParallelCommandsOutput(int count, const char **commands) {
+char *parallelgetoutput(int count, const char **commands) {
+    int *pids = malloc(count * sizeof(pid_t));
+    int **pipes = malloc(count * sizeof(int*));
     char *output = NULL;
     size_t totalSize = 0;
 
     for (int i = 0; i < count; i++) {
-        char *cmdOutput = captureCommandOutput(commands[i]);
+        pipes[i] = malloc(2 * sizeof(int));
+        if (pipe(pipes[i]) != 0) continue;
+        
+        pid_t pid = fork();
+        if (pid == 0) {
+            close(pipes[i][0]);
+            dup2(pipes[i][1], STDOUT_FILENO);
+            execl("/bin/sh", "sh", "-c", commands[i], NULL);
+            _exit(EXIT_FAILURE);
+        } else if (pid > 0) {
+            close(pipes[i][1]);
+            pids[i] = pid;
+        }
+    }
+
+    for (int i = 0; i < count; i++) {
+        char *cmdOutput = readOutputFromPipe(pipes[i][0]);
         if (cmdOutput) {
             size_t outputLen = strlen(cmdOutput);
             char *newOutput = realloc(output, totalSize + outputLen + 1);
             if (!newOutput) {
-                perror("realloc");
                 free(output);
                 free(cmdOutput);
-                return NULL;
+                break;
             }
             output = newOutput;
             memcpy(output + totalSize, cmdOutput, outputLen);
@@ -77,7 +88,12 @@ char *captureParallelCommandsOutput(int count, const char **commands) {
             output[totalSize] = '\0';
             free(cmdOutput);
         }
+        close(pipes[i][0]);
+        free(pipes[i]);
+        waitpid(pids[i], NULL, 0);
     }
 
+    free(pipes);
+    free(pids);
     return output;
 }
